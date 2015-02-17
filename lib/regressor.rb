4de285ed1e3bb341a73d6ce697f65bb7b7e5cf91
Erwin Schens <erwinschens@uni-koblenz.de>
2015-02-17 13:35:28 +0100

    added config block initializer

--- lib/regressor.rb
+++ lib/regressor.rb
@@ -1,2 +1,18 @@
 module Regressor
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :regression_path
+
+    def initialize
+      @regression_path = 'spec/models/regression'
+    end
+  end
 end
