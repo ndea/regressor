918db19d298ae6753be7880552700ed671146ace
Erwin Schens <erwinschens@uni-koblenz.de>
2015-02-17 21:03:08 +0100

    added basic exception handling

--- lib/regression_model.rb
+++ lib/regression_model.rb
@@ -9,19 +9,19 @@ class RegressionModel
   def belong_to_relations
     @model.constantize.reflect_on_all_associations(:belongs_to).map(&:name).map do |relation|
       "it { is_expected.to belong_to :#{relation}}"
-    end.join("\n\t")
+    end.join("\n\t") rescue nil
   end
 
   def has_one_relations
     @model.constantize.reflect_on_all_associations(:has_one).map(&:name).map do |relation|
       "it { is_expected.to have_one :#{relation}}"
-    end.join("\n\t")
+    end.join("\n\t") rescue nil
   end
 
   def has_many_relations
     @model.constantize.reflect_on_all_associations(:has_many).map(&:name).map do |relation|
       "it { is_expected.to have_many :#{relation}}"
-    end.join("\n\t")
+    end.join("\n\t") rescue nil
   end
 
   def validators
@@ -42,7 +42,7 @@ class RegressionModel
           validator_specs << "it { is_expected.not_to allow_value(Faker::Lorem.characters(#{maximum + 1})).for :#{attribute} }" if maximum
         end
       end
-    end
+    end rescue []
     validator_specs.compact.uniq.join("\n\t")
   end
 
@@ -50,20 +50,20 @@ class RegressionModel
     if @model.constantize.nested_attributes_options.present?
       @model.constantize.nested_attributes_options.keys.map do |key|
         "it { is_expected.to accept_nested_attributes_for :#{key} }"
-      end.join("\n\t")
+      end.join("\n\t") rescue nil
     end
   end
 
   def database_columns
     @model.constantize.columns.map(&:name).map do |column|
       "it { is_expected.to have_db_column :#{column} }"
-    end.join("\n\t") rescue ''
+    end.join("\n\t") rescue nil
   end
 
   def database_indexes
     ActiveRecord::Base.connection.indexes(@model.tableize.gsub("/", "_")).map do |indexes|
         "it { is_expected.to have_db_index #{indexes.columns}}"
-    end.flatten.join("\n\t")
+    end.flatten.join("\n\t") rescue nil
   end
 
 end
