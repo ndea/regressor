3d2e2e7978b99a8d69b8d3bb185ae6c658ca2aaa
Erwin Schens <erwinschens@uni-koblenz.de>
2015-02-18 13:01:13 +0100

    added usage comments to initializer

--- lib/generators/templates/regressor.rb
+++ lib/generators/templates/regressor.rb
@@ -1,4 +1,10 @@
+# If regressor is only in the :test group wrap your initializer in
+# if defined?(Regressor) do .. end
 Regressor.configure do |config|
-  config.regression_path = 'spec/models/regression'
-  config.excluded_models = []
+  # Defines the path where the generated files will be placed
+  # config.regression_path = 'spec/models/regression'
+
+  # Exclude Models for regression spec generation.
+  # Provide model names as String (e.g. 'User')
+  # config.excluded_models = []
 end
