f2409fca72eba86b9d539c77b5695e9fceed2910
Erwin Schens <erwinschens@uni-koblenz.de>
2015-03-07 15:19:33 +0100

    added config option in installer

--- lib/generators/templates/regressor.rb
+++ lib/generators/templates/regressor.rb
@@ -7,4 +7,9 @@ Regressor.configure do |config|
   # Exclude Models for regression spec generation.
   # Provide model names as String (e.g. 'User')
   # config.excluded_models = []
+
+  # If you are using enums in Rails 4 enable this option to generate regression specs for enums.
+  # If your Rails version is =< Rails 3 set this option to false.
+  # Default this option is set to true.
+  # config.include_enums = true
 end
