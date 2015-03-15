77f4a382eb907f1986c620972e2d7dfcace0bce3
Erwin Schens <erwinschens@uni-koblenz.de>
2015-03-15 23:56:52 +0100

    added rescue to routes generator and bumped up version to 0.3.1

--- lib/generators/regressor/controller/routing/rest/routes.rb
+++ lib/generators/regressor/controller/routing/rest/routes.rb
@@ -4,35 +4,40 @@ module Regressor
       module Rest
         module Routes
           def rest_routes
+
             controller_path = @controller.constantize.controller_path
             @controller.constantize.action_methods.map do |action_method|
-              journey_route = Rails.application.routes.routes.routes.select do |route|
-                if route.defaults.present?
-                  route.defaults[:controller].to_sym == controller_path.to_sym && route.defaults[:action].to_sym == action_method.to_sym
-                else
-                  false
+              begin
+                journey_route = Rails.application.routes.routes.routes.select do |route|
+                  if route.defaults.present?
+                    route.defaults[:controller].to_sym == controller_path.to_sym && route.defaults[:action].to_sym == action_method.to_sym
+                  else
+                    false
+                  end
+                end.first
+
+                required_parts = journey_route.required_parts.inject({}) do |required_part_hash, required_part|
+                  required_part_hash.merge!({
+                                                required_part => 1
+                                            })
                 end
-              end.first
+                url = url_for({controller: controller_path, action: action_method, only_path: true}.merge(required_parts))
 
-              required_parts = journey_route.required_parts.inject({}) do |required_part_hash, required_part|
-                required_part_hash.merge!({
-                                              required_part => 1
-                                          })
+                if journey_route.constraints[:request_method].match('GET')
+                  "it { should route(:get, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+                elsif journey_route.constraints[:request_method].match('POST')
+                  "it { should route(:post, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+                elsif journey_route.constraints[:request_method].match('PUT')
+                  "it { should route(:put, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+                elsif journey_route.constraints[:request_method].match('PATCH')
+                  "it { should route(:patch, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+                elsif journey_route.constraints[:request_method].match('DELETE')
+                  "it { should route(:delete, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+                end
+              rescue
+                puts "Could not generate regression spec for controller #{controller_path} with action: #{action_method}"
+                nil
               end
-              url = url_for({controller: controller_path, action: action_method, only_path: true}.merge(required_parts))
-
-              if journey_route.constraints[:request_method].match('GET')
-                "it { should route(:get, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
-              elsif journey_route.constraints[:request_method].match('POST')
-                "it { should route(:post, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
-              elsif journey_route.constraints[:request_method].match('PUT')
-                "it { should route(:put, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
-              elsif journey_route.constraints[:request_method].match('PATCH')
-                "it { should route(:patch, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
-              elsif journey_route.constraints[:request_method].match('DELETE')
-                "it { should route(:delete, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
-              end
-
             end.uniq.compact.join("\n\t")
           end
         end
