b4cbbf0a588d549e95a9c05e77370d85cd342b15
Erwin Schens <erwinschens@uni-koblenz.de>
2015-03-15 23:00:04 +0100

    removed dummy app from version controll and added controller regression specs

--- lib/generators/regressor/controller/routing/rest/routes.rb
+++ lib/generators/regressor/controller/routing/rest/routes.rb
@@ -5,25 +5,34 @@ module Regressor
         module Routes
           def rest_routes
             controller_path = @controller.constantize.controller_path
-            url_for(controller: controller_path, action: 'new', only_path: true)
-            @controller.constantize.action_methods.map do |method|
-              case method
-                when 'index'
-                  "it { should route(:get, '#{url_for(controller: controller_path, action: method, only_path: true)}').to(action: :index) }"
-                when 'show'
-                  "it { should route(:get, '#{url_for(controller: controller_path, action: method, id: 1, only_path: true)}').to(action: :show, id: 1) }"
-                when 'update'
-                  "it { should route(:patch, '#{url_for(controller: controller_path, action: method, id: 1, only_path: true)}').to(action: :update, id: 1) }"
-                  "it { should route(:put, '#{url_for(controller: controller_path, action: method, id: 1, only_path: true)}').to(action: :update, id: 1) }"
-                when 'destroy'
-                  "it { should route(:delete, '#{url_for(controller: controller_path, action: method, id: 1, only_path: true)}').to(action: :destroy, id: 1) }"
-                when 'create'
-                  "it { should route(:post, '#{url_for(controller: controller_path, action: method, only_path: true)}').to(action: :create) }"
-                when 'edit'
-                  "it { should route(:get, '#{url_for(controller: controller_path, action: method, id: 1, only_path: true)}').to(action: :edit, id: 1) }"
-                when 'new'
-                  "it { should route(:get, '#{url_for(controller: controller_path, action: method, only_path: true)}').to(action: :new) }"
+            @controller.constantize.action_methods.map do |action_method|
+              journey_route = Rails.application.routes.routes.routes.select do |route|
+                if route.defaults.present?
+                  route.defaults[:controller].to_sym == controller_path.to_sym && route.defaults[:action].to_sym == action_method.to_sym
+                else
+                  false
+                end
+              end.first
+
+              required_parts = journey_route.required_parts.inject({}) do |required_part_hash, required_part|
+                required_part_hash.merge!({
+                                              required_part => 1
+                                          })
               end
+              url = url_for({controller: controller_path, action: action_method, only_path: true}.merge(required_parts))
+
+              if journey_route.constraints[:request_method].match('GET')
+                "it { should route(:get, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+              elsif journey_route.constraints[:request_method].match('POST')
+                "it { should route(:post, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+              elsif journey_route.constraints[:request_method].match('PUT')
+                "it { should route(:put, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+              elsif journey_route.constraints[:request_method].match('PATCH')
+                "it { should route(:patch, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+              elsif journey_route.constraints[:request_method].match('DELETE')
+                "it { should route(:delete, '#{url}').to(#{{controller: controller_path, action: action_method}.merge(required_parts).to_s}) } "
+              end
+
             end.uniq.compact.join("\n\t")
           end
         end
