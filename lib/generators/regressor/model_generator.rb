505ddc0c1e312d7a79a5d8c1d1dd782acb89969e
Erwin Schens <erwinschens@uni-koblenz.de>
2015-08-03 13:58:20 +0200

    add mongoid model loading

--- lib/generators/regressor/model_generator.rb
+++ lib/generators/regressor/model_generator.rb
@@ -6,7 +6,14 @@ module Regressor
 
     def create_regression_files
       Rails.application.eager_load!
-      ActiveRecord::Base.descendants.map(&:name).reject { |x| Regressor.configuration.excluded_models.include? x }.each do |model|
+
+      models = if Regressor.configuration.orm == :mongoid
+                 load_mongoid_models
+               else
+                 load_ar_models
+               end
+
+      models.each do |model|
         begin
           @model = Regressor::RegressionModel.new(model)
           create_file "#{Regressor.configuration.regression_path}/#{model.tableize.gsub("/", "_").singularize}_spec.rb", ERB.new(File.new(File.expand_path('../templates/spec_regression_template.erb', File.dirname(__FILE__))).read).result(binding)
@@ -14,6 +21,19 @@ module Regressor
           puts "Cannot create Model Regression for: #{model}"
         end
       end
+    end
+
+    def load_ar_models
+      ActiveRecord::Base.descendants.map(&:name).reject { |x| Regressor.configuration.excluded_models.include? x }
+    end
+
+    def load_mongoid_models
+      models = Object.constants.collect { |sym| Object.const_get(sym) }.
+          select { |constant| constant.class == Class && constant.include?(Mongoid::Document) }
+      models.each do |model|
+        models << model.subclasses
+      end
+      models.flatten
     end
 
   end
