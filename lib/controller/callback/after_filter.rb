c0bc9922f4b604f274258318a9b615ad03038eb9
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-08-07 12:18:10 -0400

    Using ":" in front of the raw_filter name on callbacks only if the filter is a Symbol, otherwise use just raw_filter.to_s.

--- lib/controller/callback/after_filter.rb
+++ lib/controller/callback/after_filter.rb
@@ -4,7 +4,7 @@ module Regressor
       module AfterFilter
         def after_callbacks
           after_filters.map do |filter_name|
-            "it { should use_after_filter(:#{filter_name}) }"
+            "it { should use_after_filter(#{filter_name}) }"
           end.compact.uniq.join("\n  ")
         end
       end
